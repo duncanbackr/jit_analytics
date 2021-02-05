6215fb7fef7886aa2880f84313b8d976ee98d1c2
duncanbackr <duncan@backr.space>
2021-02-05 13:40:06 -0500

    first commit

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 from datetime import datetime
-import Query
-import Analytics
-import sort
+import Query, Analytics, Sort, Formating
 
-if __name__ == '__main__':
-     creator_id = 37 # TODO grab these from the request params
-     limit = 5000
+def main(request):
+
+     requestArgs = request.args()
+
+     if requestArgs.get('creator_id'):
+          creator_id = int(requestArgs.get('creator_id'))
+     else: 
+          # TODO raise error with frontend (ask Michael)
+          return {'error': 'must include a creator_id'}
+
+     if request.args.get('limit'):
+          limit = int(requestArgs.get('limit'))
+     else:
+          limit = 200
      
      """ Get data from db and backrest """
      raw_data = Query.latest_comments(creator_id, limit)
      cut_off_data = Query.batch_data(creator_id)
 
      """ Add analytics calculations """
-     all_comments = Analytics.add_score(raw_data, cut_off_data)
+     scored_comments, replies = Analytics.add_score(raw_data, cut_off_data)
+
+     """ Filter Data """
+     # TODO call filter
+     filtered_comments = scored_comments
+
+     """ Sort Data """
+     # TODO call sort
+     sorted_comments = filtered_comments
+
+     """ Format Data """
+     if requestArgs.get('')
+     formated_result = Formating.comments(sorted_comments, replies)
      
-     print(all_comments)
+     return formated_result
