9c5e2dc63a188c18a2b9b708e7e161fb85f86225
ecavan <eli_cavan@live.ca>
2021-02-09 19:27:09 -0500

    filter test

--- Tests/test_integration/test_flask.py
+++ Tests/test_integration/test_flask.py
@@ -1,8 +1,10 @@
 import pytest
+import Analytics, Filter, Formating, Query, Sort
 from main import main
 from Tests.test_integration.fixtures import Flask_Request
 from Tests.test_integration.raw_data_comments import raw_data_comments
 from Tests.test_integration.raw_data_fans import raw_data_f
+from Tests.test_integration.raw_data_filter import raw_data_filter
 
 
 def test_home():
@@ -17,3 +19,13 @@ def test_fans(mocker):
         return_value=raw_data_comments)
     response = main(flask_request)
     assert response == raw_data_f
+
+
+def test_filter(mocker):
+    flask_request = Flask_Request({'okta_id':'00u10v74k6FsEfLFP4x7', 'resource':'comments'})
+    mocker.patch('Query.latest_comments',
+        return_value=raw_data_comments)
+    response = main(flask_request)
+    response = Filter.from_list(response, badge='topFan')
+    assert response == raw_data_filter
+
