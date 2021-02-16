a9f2f164b5b9afee98ae29800d08181d858cec6b
ecavan <eli_cavan@live.ca>
2021-02-16 15:24:49 -0500

    fixed timedelta issue

--- Analytics/delayscores.py
+++ Analytics/delayscores.py
@@ -1,14 +1,14 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def create_delay_scores(last_comment, sec_comment, time_now):
     '''subtracts dates in dataframe from todays date and converts 
     to # of days with logic for if the value is a nan'''
 
-    delay1 = (time_now - last_comment).days 
+    delay1 = (time_now - last_comment).days + ((time_now - last_comment).seconds)/(3600*24)
 
     if sec_comment:
-        delay2 = (last_comment - sec_comment).days
-        delay3 = (time_now - sec_comment).days
+        delay2 = (last_comment - sec_comment).days+ ((last_comment - sec_comment).seconds)/(3600*24)
+        delay3 = (time_now - sec_comment).days+ ((time_now - sec_comment).seconds)/(3600*24)
         return [delay1, delay2, delay3]
 
     else:
