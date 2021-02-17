bd8067b95dc885a07d29cd17bd86604eda74cb3e
ecavan <eli_cavan@live.ca>
2021-02-17 15:47:14 -0500

    working on removing none function for delay3

--- Analytics/badgescore.py
+++ Analytics/badgescore.py
@@ -2,12 +2,6 @@ from datetime import datetime
 from Analytics.delayscores import create_delay_scores_scaled
 from Analytics.scaler import min_max_scaler
 
-def remove_none(value):
-    if str(value) == 'None':
-        return 0
-    else:
-        return value
-
 def add_badge_score(total_comments, top_fan_cutoff, total_responses, badge, delays, maxs, mins):
     '''takes in parameters and outputs badge score'''
     
@@ -20,7 +14,7 @@ def add_badge_score(total_comments, top_fan_cutoff, total_responses, badge, dela
         return min_max_scaler(total_comments, maxs['total_comments'], mins['total_comments']) - delay1_scaled
 
     elif badge == 'reEngageFan':
-        return  remove_none(delay2_scaled) - delay1_scaled
+        return  delay2_scaled - delay1_scaled
 
     else:
         return min_max_scaler(total_comments, maxs['total_comments'], mins['total_comments'])
--- Analytics/retentionscore.py
+++ Analytics/retentionscore.py
@@ -3,7 +3,7 @@ from Analytics.delayscores import create_delay_scores_scaled
 from Analytics.scaler import min_max_scaler
 
 def remove_none(value):
-    if str(value) == 'None':
+    if value is None:
         return 0
 
     else:
@@ -15,8 +15,8 @@ def add_retention(total_comments, top_fan_cutoff, total_responses, total_replies
     delay1_scaled, delay2_scaled, delay3_scaled = delays_scaled
     
     if badge == 'New Fan':
-        return min_max_scaler(total_comments, maxs['total_comments'], mins['total_comments']) - min_max_scaler(total_responses, maxs['total_responses'], mins['total_responses']) - delay1_scaled + \
-                            remove_none(delay3_scaled) - 1 + min_max_scaler(total_replies, maxs['total_replies'], mins['total_replies'])
+        return min_max_scaler(total_comments, maxs['total_comments'], mins['total_comments']) - min_max_scaler(total_responses, maxs['total_responses'], mins['total_responses']) - delay1_scaled  \
+                             - 1 + min_max_scaler(total_replies, maxs['total_replies'], mins['total_replies'])
     elif badge == 'reEngageFan':
         return min_max_scaler(total_comments, maxs['total_comments'], mins['total_comments']) - min_max_scaler(total_responses, maxs['total_responses'], mins['total_responses']) - delay1_scaled  + \
                                 remove_none(delay3_scaled) + 2 + min_max_scaler(total_replies, maxs['total_replies'], mins['total_replies'])
