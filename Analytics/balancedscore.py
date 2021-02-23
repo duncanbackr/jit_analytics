79a835c83d04e227e7286e1fe6c7bd9f99abf431
ecavan <eli_cavan@live.ca>
2021-02-23 13:49:03 -0500

    deploying to test further

--- Analytics/balancedscore.py
+++ Analytics/balancedscore.py
@@ -8,16 +8,16 @@ def add_balanced(total_comments, top_fan_cutoff, total_responses, total_replies,
     delay1_scaled, delay2_scaled, delay3_scaled = delays_scaled
 
     if badge == 'newFan':
-        return (1-growth) + retention 
+        return (1-growth) + retention - 0.5
     
     elif badge == 'trendingFan':
-        return (1-growth) + retention 
+        return (1-growth) + retention - 0.25
 
     elif badge == 'reEngageFan':
-        return retention + (1-growth) + 1.2
+        return retention + (1-growth) 
 
     elif badge == 'topFan':
-        return retention + (1-growth)
+        return retention + (1-growth) + 0.2
     else:
         return retention + (1-growth) - 1
 
--- Analytics/growthscore.py
+++ Analytics/growthscore.py
@@ -14,5 +14,5 @@ def add_growth(total_comments, top_fan_cutoff, total_responses, badge, delays_sc
     elif badge == 'trendingFan':
         return 2*delay1_scaled+min_max_scaler(total_responses, maxs['total_responses'], mins['total_responses'])+ delay3_scaled
     else:
-        return min_max_scaler(total_comments, maxs['total_comments'], mins['total_comments']) + 0.5 + \
+        return min_max_scaler(total_comments, maxs['total_comments'], mins['total_comments']) + 0.9 + \
                             min_max_scaler(total_responses, maxs['total_responses'], mins['total_responses']) + 2*delay1_scaled
