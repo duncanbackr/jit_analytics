5a4b23eea68c6941777e554936ab9e22112369c6
duncanbackr <duncan@backr.space>
2021-03-12 12:12:01 -0500

    add fan_id filter to comments resource

--- Query/parse.py
+++ Query/parse.py
@@ -1,4 +1,4 @@
-def create_string(videoId):
+def create_video_string(videoId):
     """
     Function which accepts a videoId arg and returns sql string
 
@@ -12,3 +12,15 @@ def create_string(videoId):
     for video_id in video_list:
         video_string = video_string + str(video_id) + ', '
     return video_string[:-4] + ')'
+
+def create_fan_string(fan_id):
+    """
+    Function which accepts a fan_id arg and returns sql string
+
+    param:
+        fan_id = '1234'
+    returns:
+        " AND youtube_fan_id = 1234"
+    """
+    video_string = f" AND youtube_fan_id = {fan_id}"
+    return video_string
--- Tests/test_query/test_parse.py
+++ Tests/test_query/test_parse.py
@@ -1,8 +1,8 @@
 import Query
 
 def test_video_id_parser():
-    video_string = Query.parse.create_string('12z13z14z')
+    video_string = Query.parse.create_video_string('12z13z14z')
     assert video_string == ' AND video_id in (12, 13, 14)'
 
-    video_string = Query.parse.create_string('12z')
+    video_string = Query.parse.create_video_string('12z')
     assert video_string == ' AND video_id in (12)'
