e4cf9b4ddc3dfc13a08034aceb89443a15e842a1
ecavan <eli_cavan@live.ca>
2021-03-23 16:17:24 -0400

    all tests passed

--- Sort/__init__.py
+++ Sort/__init__.py
@@ -2,7 +2,7 @@ def from_list(proccessed_comments, param = 'balanced'):
     '''takes in param and sorts by 'timestamp', 'balanced', 'growth', 'retention'
     default is the balanced score; can take param = None which returns the default'''
     
-    reverse_dict ={'growth':False, 'retention':True, 'timestamp':True, 'badge_score':True, 'balanced':True}
+    reverse_dict ={'growth':True, 'retention':True, 'timestamp':True, 'badge_score':True, 'balanced':True}
     
     return sorted(proccessed_comments, key=lambda k : k[param], reverse = reverse_dict[param]) 
 
--- Tests/test_scores/test_scores.py
+++ Tests/test_scores/test_scores.py
@@ -10,13 +10,13 @@ def test_badge_Trend(trend_fan):
     assert None ==  trend_fan
 
 def test_score_growth(growth):
-    assert 2.343173678837946 ==  growth
+    assert 6.183568462877808 ==  growth
 
 def test_score_retention(retention):
-    assert -2.1647605182569194 ==  retention
+    assert 2.721586839418973 ==  retention
 
 def test_sort_growth(sort_growth):
-    assert [{'growth':2, 'balanced':3}, {'growth':5, 'balanced':4}, {'growth':6, 'balanced':8}] == sort_growth
+    assert [{'growth':6, 'balanced':8}, {'growth':5, 'balanced':4}, {'growth':2, 'balanced':3}] == sort_growth
 
 def test_filter_badge(filter_badge):
     assert [{'growth':2, 'badge':'topFan'}, {'growth':5, 'badge':'topFan'}] == filter_badge
