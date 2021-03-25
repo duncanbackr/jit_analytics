257b33a2f4807655238506b230371c1b7719ceae
ecavan <eli_cavan@live.ca>
2021-03-25 15:09:10 -0400

    change 7  to score

--- Analytics/balancedscore.py
+++ Analytics/balancedscore.py
@@ -3,4 +3,4 @@ from datetime import datetime
 def add_balanced(total_comments, top_fan_cutoff, responses, total_replies, badge, delays_scaled, maxs, mins, retention, growth):
     '''takes in parameters and outputs growth scores'''
 
-    return growth + retention
+    return 1.5*growth + retention
