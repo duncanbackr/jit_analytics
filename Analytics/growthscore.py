1e8fffb8c7b88c910718d80834d5227080f3be85
ecavan <eli_cavan@live.ca>
2021-03-25 14:29:28 -0400

    change 4 to score

--- Analytics/growthscore.py
+++ Analytics/growthscore.py
@@ -7,10 +7,10 @@ def add_growth(total_comments, top_fan_cutoff, responses, badge, delays_scaled, 
     delay1_scaled, delay2_scaled, delay3_scaled = delays_scaled
 
     if delay2_scaled:
-        return - delay1_scaled
+        return 1/delay1_scaled
 
     else:
-        return - delay1_scaled \
+        return 1/delay1_scaled \
 
 
     
